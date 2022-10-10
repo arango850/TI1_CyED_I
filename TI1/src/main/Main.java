8c29477d98ba0811c55138385ed6f34648198214
Andres Arango <arango850@gmail.com>
2022-10-10 15:11:37 -0500

    Creacion de la clase main

---
+++ TI1/src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner scanner;
+	
+	public Main() {
+		scanner = new Scanner(System.in);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		setMain(new Main());
+		Main.menu();
+	}
+
+	private static void menu() {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+	}
+
+	private static void setMain(Main main) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
